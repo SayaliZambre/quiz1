e8fb39d15c413f3c6ec1419a206d2fcacb16fd37
blockdev09 <135862086+blockdev09@users.noreply.github.com>
2023-11-07 20:31:14 +0530

    route the Leaderboard page and result page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route} from "react-router-dom";
+import Leaderboard from './components/leaderboard';
+import Result from './components/result';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/Leaderboard' element={<Leaderboard/>}></Route>
+      <Route path='/Result' element={<Result/>}></Route>
+    </Routes>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
   );
 }
-
 export default App;
---
+++ src/components/leaderboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Leaderboard() {
+  return (
+    <div>
+        <h1>This is Leaderboard Page.</h1>
+    </div>
+  )
+}
