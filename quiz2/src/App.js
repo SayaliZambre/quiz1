e7cc9b6f2b737d952db2d8e2e5e1ce43fb423de9
vivekbisen04 <121126140+vivekbisen04@users.noreply.github.com>
2023-11-08 10:37:12 +0530

    route
    route

---
+++ quiz2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Que from './components/Que'
+
+function App() {
+  return (
+    <div>
+      <Que/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ quiz2/src/components/Que.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text} from 'react'
+function Que() {
+  return (
+    <div>
+       <div className='title'>
+         Quiz
+       </div>
+
+    </div>
+  )
+}
+
+export default Que
