5388722adb23109f85e4f323871ba9ce43362674
blockdev09 <135862086+blockdev09@users.noreply.github.com>
2023-11-12 08:58:51 +0530

    pushed leaderboard page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {Routes,Route} from "react-router-dom";
-import Leaderboard from './components/leaderboard';
+import Leaderboard from './components/Leaderboard/Leaderboard';
 import Result from './components/result';
 function App() {
   return (
     <Routes>
-      <Route path='/Leaderboard' element={<Leaderboard />}></Route>
+      <Route path='/' element={<Leaderboard />}></Route>
       <Route path='/Result' element={<Result />}></Route>
     </Routes>
   );
--- src/components/leaderboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Leaderboard() {
-  return (
-    <div>
-        <h1>This is Leaderboard Page.</h1>
-    </div>
-  )
-}
